83582ae073cc78b1d34245a5ef6e485729d51b73
blackmarllbor0 <3100194@gmail.com>
2023-09-18 19:36:51 +0600

    BaseController for easier control of other controllers and not subject to DRY error

---
+++ src/app/base.controller.ts
@@ -0,0 +1,28 @@
+import { Response, Router } from 'express';
+
+export abstract class BaseController {
+  public router: Router;
+
+  constructor(public readonly path: string) {
+    this.router = Router();
+  }
+
+  abstract initRoutes(): void;
+
+  public json(res: Response, code: number, message: string): Response {
+    return res.status(code).json({ message });
+  }
+
+  public ok<T>(res: Response, dto?: T): Response<T | void> {
+    if (dto) {
+      res.type('application/json');
+      return res.status(200).json(dto);
+    }
+
+    return res.status(200);
+  }
+
+  public created<T>(res: Response, data?: T): Response<T> {
+    return res.status(201).json(data);
+  }
+}
