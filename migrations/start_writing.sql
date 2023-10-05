e7a832ad6a3d1dbd18dc83b489eb7c37773a6e71
blackmarllbor0 <3100194@gmail.com>
2023-10-05 18:48:50 +0600

    add default values to migrations

---
+++ migrations/start_writing.sql
@@ -0,0 +1,113 @@
+INSERT INTO "user_status" us ("name") VALUES ('online');
+
+INSERT INTO "user_role" ur ("name") VALUES ('customer');
+
+INSERT INTO "user_role" ur ("name") VALUES ('admin');
+
+INSERT INTO "auth_audit_event" aae ("name") VALUES ('successfully_login');
+
+INSERT INTO "auth_audit_event" aae ("name") VALUES ('failed_password');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Building Materials');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (1, 'Lumber and Wood Products');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (1, 'Concrete and Cement');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (1, 'Bricks and Blocks');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (1, 'Roofing Materials');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (1, 'Insulation and Soundproofing');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Tools and Equipment');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (2, 'Power Tools');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (2, 'Hand Tools');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (2, 'Safety Equipment');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (2, 'Construction Machinery');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (2, 'Hardware and Fasteners');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Paint and Supplies');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (3, 'Paints and Primers');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (3, 'Brushes and Rollers');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (3, 'Paint Thinners and Solvents');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (3, 'Tarps and Drop Cloths');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Electrical and Lighting');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (4, 'Electrical Wiring and Cables');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (4, 'Lighting Fixtures');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (4, 'Electrical Panels and Breakers');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (4, 'Batteries and Power Supplies');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Plumbing and Fixtures');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (5, 'Pipes and Fittings');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (5, 'Faucets and Sinks');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (5, 'Toilets and Toilet Seats');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (5, 'Showerheads and Tub Faucets');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Flooring and Tiles');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (6, 'Hardwood Flooring');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (6, 'Tile Flooring');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (6, 'Carpet and Rugs');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (6, 'Flooring Accessories');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Outdoor and Garden');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (7, 'Outdoor Furniture');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (7, 'Gardening Tools');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (7, 'Landscaping Materials');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (7, 'Pools and Spas');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Home Improvement');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (8, 'Kitchen and Bathroom Remodeling');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (8, 'Home Décor');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (8, 'Home Security');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (8, 'Storage and Organization');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Hardware and Fasteners');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (9, 'Nails and Screws');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (9, 'Bolts and Nuts');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (9, 'Anchors and Hooks');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (9, 'Hinges and Latches');
+
+INSERT INTO "category_type" ct ("name") VALUES ('Heating and Cooling');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (10, 'Heating Systems');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (10, 'Air Conditioning');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (10, 'Fans and Ventilation');
+
+INSERT INTO "category" c ("category_type_id", "name") VALUES (10, 'Fireplace and Stove Accessories');
